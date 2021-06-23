923a1b10e79b77c475ad42ebf89de6594a1f93df
future668 <274239431@qq.com>
2021-06-23 13:01:25 +0800

    test02

--- src/main/java/com/dd/git/GitTest.java
+++ src/main/java/com/dd/git/GitTest.java
@@ -13,6 +13,7 @@ public class GitTest {
         System.out.println("push test2222");
         System.out.println("push test37,mm7");
         System.out.println("push mmmmmmmmmmmmm7");
+        System.out.println("push 4");
 
     }
 }
